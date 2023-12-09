bc1c9fa9ddda9d30ec9c3d68a6a261ab3938f765
TatChungLee <133053710+TatChungLee@users.noreply.github.com>
2023-12-09 16:27:18 -0500

    First Commit

---
+++ interface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP212_FinalTest
+{
+    public interface ITransaction
+    {
+        void Withdraw(double amount, Person person);
+        void Deposit(double amount, Person person);
+    }
+
+}
